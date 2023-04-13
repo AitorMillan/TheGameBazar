d69d53890087dc5051bbb8bc9b8283e0097273cf
Aitor Millán Trujillo <91531456+AitorMillan@users.noreply.github.com>
2023-04-13 17:38:54 +0200

    Actualizados tests
    -Actualizado el logging de cada test para ser más descriptivo.
    -Arreglado un test que a veces fallaba debido a la API de CheapShark

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,8 +9,8 @@ def client():
 
 
 def test_obtener_juego (client):
-    logging.info("Iniciando test Obtener Juego")
-    respuesta = client.put('obtenerJuegos/Outlast')
+    logging.info("Iniciando test para obtener un juego")
+    respuesta = client.put('obtenerJuegos/Grand')
     assert respuesta.status_code == 200
 
 
@@ -21,34 +21,34 @@ def test_juego_no_existente (client):
 
 
 def test_inicio_sesion_incorrecto(client):
-    logging.info("Iniciando test Inicio Sesión Incorrecto")
+    logging.info("Iniciando test para un inicio de sesión incorrecto")
     respuesta = client.put('/iniciarSesion/123;456')
     assert respuesta.status_code == 500
 
 
 def test_inicio_sesion_correcto(client):
-    logging.info("Iniciando test Inicio Sesión Correcto")
+    logging.info("Iniciando test para un inicio de sesión correcto")
     respuesta = client.put('/iniciarSesion/Aitor;TGB123')
     assert respuesta.status_code == 200
 
 
 def test_registro_juego_incompleto(client):
-    logging.info("Iniciando test Registro Juego Incompleto")
+    logging.info("Iniciando test para intentar registrar un juego incompleto")
     respuesta = client.put('registrarJuego/outlast;steam')
     assert respuesta.status_code == 500
 
 def test_obtener_estadisticas(client):
-    logging.info("Iniciando test Obtener Estadísticas")
+    logging.info("Iniciando test para obtener estadísticas")
     respuesta = client.get('/obtenerEstadisticas')
     assert respuesta.status_code == 200
 
 
 def test_registro_usuario_incorrecto(client):
-    logging.info("Iniciando test Regristo Usuario Incorrecto")
+    logging.info("Iniciando test para intentar regristar un usuario de forma incorrecta")
     respuesta = client.put('/registrarUsuario/Aitor;000')
     assert respuesta.status_code == 500
 
 def test_registro_usuario_index_error(client):
-    logging.info("Iniciando test Registro Usuario Index Error")
+    logging.info("Iniciando test para comprobar que se recoge la excepción Index Error al registrar un usuario")
     respuesta = client.put('/registrarUsuario/Aitor')
     assert respuesta.status_code == 500
